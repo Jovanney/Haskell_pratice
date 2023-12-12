07526b0057f8d434270de81c6663ee00153edba7
Jovanney <100790418+Jovanney@users.noreply.github.com>
2023-12-11 22:22:49 -0300

    Feat: recurção

--- semana2.hs
+++ semana2.hs
@@ -19,5 +19,30 @@ somaDobroQuadrado2 x y = dsq x + dsq y
     where
         dsq z = 2 * (z * z)
 
--- Funções de escopo local
+
+-- Funções recursivas
+
+fatorial :: Integer -> Integer
+fatorial n 
+    | n == 0 = 1 -- caso base
+    | otherwise = n * fatorial (n - 1) --caso Recursivo
+
+fibonacci :: Integer -> Integer
+fibonacci n
+    | n == 0 = 0
+    | n == 1 = 1
+    | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
+
+-- Recurção Mútua
+
+ehpar, ehimpar :: Integer -> Bool
+ehpar n
+    | n == 0 = True
+    | otherwise = ehimpar (n - 1)
+ehimpar n
+    | n == 0 = False
+    | otherwise = ehpar (n - 1)
+
+-- Chamadas de cauda
+
 
